2e3f25546a76600cd17fb3d115e2a70193e95521
Martin Kim Dung-Pham <kim@elbedev.com>
2018-04-01 12:19:56 +0200

    Modify cell equality condition and add copy method

--- Sources/gameoflife/Cell.swift
+++ Sources/gameoflife/Cell.swift
@@ -1,6 +1,6 @@
 public class Cell: Equatable {
     public internal(set) var populated: Bool! = false
-    var neighbors: [Cell?] = [nil, nil, nil, nil, nil, nil, nil, nil]
+    var neighbors: [Cell?]
     private(set) lazy var topLeftNeighbor = neighbors[0]!
     private(set) lazy var topCenterNeighbor = neighbors[1]!
     private(set) lazy var topRightNeighbor = neighbors[2]!
@@ -9,8 +9,17 @@ public class Cell: Equatable {
     private(set) lazy var bottomCenterNeighbor = neighbors[5]!
     private(set) lazy var bottomLeftNeighbor = neighbors[6]!
     private(set) lazy var leftNeighbor = neighbors[7]!
+
+    init(neighbors: [Cell?] = [nil, nil, nil, nil, nil, nil, nil, nil], populated: Bool = false) {
+        self.neighbors = neighbors
+        self.populated = populated
+    }
     
     public static func ==(lhs: Cell, rhs: Cell) -> Bool {
-        return lhs === rhs
+        return lhs.populated == rhs.populated && lhs.neighbors == rhs.neighbors
+    }
+
+    internal func copy() -> Cell {
+        return Cell(neighbors: neighbors, populated: populated)
     }
 }
--- Tests/gameoflifeTests/CellTests.swift
+++ Tests/gameoflifeTests/CellTests.swift
@@ -9,9 +9,61 @@ class CellTests: XCTestCase {
         XCTAssertEqual(cell.neighbors.count, 8, "A cell should have 8 neighbors")
     }
 
+    func testCopy() {
+        let cell = Cell(neighbors: [], populated: false)
+        let cellCopy = cell.copy()
+        XCTAssertEqual(cell, cellCopy)
+    }
+
+    func testCopyCopiesNeighbors() {
+        let cell = Cell(neighbors: [Cell(), Cell()], populated: true)
+        let cellCopy = cell.copy()
+        XCTAssertEqual(cell, cellCopy)
+
+        let cellCopyWithModifiedNeighbors = cell.copy()
+        let populatedCell = Cell(populated: true)
+        cell.neighbors[0] = populatedCell
+        XCTAssertNotEqual(cell, cellCopyWithModifiedNeighbors)
+    }
+
     func testCellIsUnpopulatedByDefault() {
         XCTAssertFalse(cell.populated, "A cell should be unpopulated by default")
     }
+
+    func testEqualityWithSameNeighbors() {
+        let populatedNeighbor = Cell(populated: true)
+        cell.neighbors[1] = populatedNeighbor
+        let neighbors = cell.neighbors
+        let otherCell = Cell(neighbors: neighbors, populated: cell.populated)
+        XCTAssertEqual(cell, otherCell)
+    }
+
+    func testInEqualityWithDifferentNeighbors() {
+        let populatedNeighbor = Cell(populated: true)
+        cell.neighbors[1] = populatedNeighbor
+        let otherCell = Cell(populated: cell.populated)
+        XCTAssertNotEqual(cell, otherCell)
+    }
+
+    func testEqualityOfPopulatedState() {
+        cell.populated = true
+        let otherCell = Cell(populated: true)
+        XCTAssertEqual(cell, otherCell)
+    }
+
+    func testInEqualityOfPopulatedState() {
+        let otherCell = Cell(populated: true)
+        XCTAssertNotEqual(cell, otherCell)
+    }
     
-    static var allTests = [("testCellHas8Neighbors", testCellHas8Neighbors)]
+    static var allTests = [
+        ("testCellHas8Neighbors", testCellHas8Neighbors),
+        ("testCopy", testCopy),
+        ("testCopyCopiesNeighbors", testCopyCopiesNeighbors),
+        ("testCellIsUnpopulatedByDefault", testCellIsUnpopulatedByDefault),
+        ("testEqualityWithSameNeighbors", testEqualityWithSameNeighbors),
+        ("testInEqualityWithDifferentNeighbors", testInEqualityWithDifferentNeighbors),
+        ("testEqualityOfPopulatedState", testEqualityOfPopulatedState),
+        ("testInEqualityOfPopulatedState", testInEqualityOfPopulatedState),
+        ]
 }
